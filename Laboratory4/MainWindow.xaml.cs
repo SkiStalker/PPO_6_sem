e71aedece8431cc7835b5f2a38b906e5e8b5139b
Никита Забелин <asdfghjkliop44@gmail.com>
2023-05-19 10:37:02 +0300

    Initial commit

---
+++ Laboratory4/MainWindow.xaml.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratory4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string? a;
+        private string? n;
+        private string? x1;
+        private string? dx;
+        private string? rows;
+        private string? columns;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private object? matrix;
+        private string? functionResult;
+
+        public object? Matrix
+        {
+            get => matrix;
+            set
+            {
+                matrix = value;
+                OnPropertyChanged("Matrix");
+            }
+        }
+
+        public string? FunctionResult
+        {
+            get => functionResult;
+            set
+            {
+                functionResult = value;
+                OnPropertyChanged("FunctionResult");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void CalcFunction()
+        {
+            DataView? dataView = (DataView?)Matrix;
+            if (dataView is { Table: { } })
+            {
+                int topZeros = 0;
+                int bottomZeros = 0;
+                int rowsCount = dataView.Table.Rows.Count;
+                int columnsCount = dataView.Table.Columns.Count;
+                for (int i = 0; i < rowsCount / 2; i++)
+                {
+                    for (int j = 0; j < columnsCount; j++)
+                    {
+                        topZeros += (double)(dataView.Table.Rows[i][j] ?? 0) == 0 ? 1 : 0;
+
+                        bottomZeros += (double)(dataView.Table.Rows[i + rowsCount / 2 + rowsCount % 2][j] ?? 0) == 0
+                            ? 1
+                            : 0;
+                    }
+                }
+
+                if (topZeros > bottomZeros)
+                {
+                    FunctionResult = "Больше в верхней";
+                }
+                else if (topZeros < bottomZeros)
+                {
+                    FunctionResult = "Больше в нижней";
+                }
+                else
+                {
+                    FunctionResult = "Одинаково";
+                }
+            }
+            else
+            {
+                FunctionResult = "Undefined";
+            }
+        }
+
+        private void SetXYGrid()
+        {
+            if (!string.IsNullOrEmpty(a) && !string.IsNullOrEmpty(n) && !string.IsNullOrEmpty(x1) &&
+                !string.IsNullOrEmpty(dx))
+            {
+                if (double.TryParse(a, out double doubleA) && double.TryParse(n, out double doubleN) &&
+                    double.TryParse(x1, out double doubleX1) && double.TryParse(dx, out double doubleDX))
+
+                {
+                    XYGrid.Items.Clear();
+                    double y = 0;
+                    double x = 0;
+                    for (int i = 0; i < doubleN; i++)
+                    {
+                        x = doubleX1 + doubleDX * i;
+                        if (x < 0)
+                        {
+                            y = Math.Sqrt(Math.Cbrt(doubleA * doubleA) - Math.Cbrt(x * x));
+                        }
+                        else
+                        {
+                            y = doubleA + Math.Sqrt(Math.Pow(x, 3) / (2 * doubleA - x));
+                        }
+
+                        XYGrid.Items.Add(new Point(x, y));
+                    }
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void SetMatrix()
+        {
+            if (!string.IsNullOrEmpty(rows) && !string.IsNullOrEmpty(columns))
+            {
+                if (int.TryParse(rows, out int intRows) && int.TryParse(columns, out int intColumns))
+                {
+                    DataView? oldDataTable = (DataView?)Matrix;
+
+                    DataTable dataTable = new DataTable();
+                    for (int i = 0; i < intColumns; i++)
+                    {
+                        dataTable.Columns.Add(new DataColumn($"c{i}", typeof(double)));
+                    }
+
+                    for (int j = 0; j < intRows; j++)
+                    {
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int c = 0; c < intColumns; c++)
+                        {
+                            if (oldDataTable is { Table: { } } &&
+                                oldDataTable.Table.Rows.Count > j && oldDataTable.Table.Rows[j].ItemArray.Length > c)
+                            {
+                                dataRow[c] = oldDataTable.Table.Rows[j][c];
+                            }
+                            else
+                            {
+                                dataRow[c] = 0;
+                            }
+                        }
+
+                        dataTable.Rows.Add(dataRow);
+                    }
+
+                    Matrix = dataTable.DefaultView;
+                }
+            }
+        }
+
+        private void A_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            a = A.Text;
+            SetXYGrid();
+        }
+
+        private void X1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            x1 = XInit.Text;
+            SetXYGrid();
+        }
+
+        private void DX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            dx = DX.Text;
+            SetXYGrid();
+        }
+
+        private void N_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            n = N.Text;
+            SetXYGrid();
+        }
+
+        private void Resize_OnClick(object sender, RoutedEventArgs e)
+        {
+            rows = Rows.Text;
+            columns = Columns.Text;
+            SetMatrix();
+            CalcFunction();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            CalcFunction();
+        }
+    }
+}
---
+++ Laboratory5/MainWindowViewModel.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using FileClientUI;
+using Microsoft.Win32;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace Laboratory5;
+
+public class MainWindowViewModel : ObservableObject
+{
+    private PlotModel? graphModel;
+    private object? matrix;
+    private ICommand? loadFileCommand;
+
+    public object? Matrix
+    {
+        get => matrix;
+        set
+        {
+            matrix = value;
+            OnPropertyChanged("Matrix");
+        }
+    }
+
+    public ICommand LoadFileCommand
+    {
+        get
+        {
+            return loadFileCommand ??= new RelayCommand(param =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Txt files |*.txt";
+
+                if (openFileDialog.ShowDialog() ?? false)
+                {
+                    using Stream stream = openFileDialog.OpenFile();
+                    using StreamReader reader = new StreamReader(stream);
+
+                    DataTable newDataTable = new DataTable();
+                    List<Point> points = new List<Point>();
+                    double sx4 = 0;
+                    double sx3 = 0;
+                    double sx2 = 0;
+                    double sx2y = 0;
+                    double sx = 0;
+                    double sxy = 0;
+                    double sy = 0;
+                    double n = 0;
+                    double s1_x = 0;
+                    double sy_x = 0;
+                    double s1_x2 = 0;
+                    double? minX = null;
+                    double? maxX = null;
+                    double? minY = null;
+                    double? maxY = null;
+
+
+                    newDataTable.Columns.Clear();
+                    newDataTable.Rows.Clear();
+                    newDataTable.Columns.Add(new DataColumn("X", typeof(double)));
+                    newDataTable.Columns.Add(new DataColumn("Y", typeof(double)));
+
+
+                    while (reader.ReadLine() is { } str)
+                    {
+                        string[] parts = str.Trim().Split(":");
+                        if (parts.Length == 2)
+                        {
+                            if (double.TryParse(parts[0].Trim(), out double x) &&
+                                double.TryParse(parts[1].Trim(), out double y))
+                            {
+                                maxX = Math.Max(maxX ??= x, x);
+                                minX = Math.Min(minX ??= x, x);
+                                maxY = Math.Max(maxY ??= y, y);
+                                minY = Math.Min(minY ??= y, y);
+                                
+                                
+                                points.Add(new Point(x, y));
+                                sx4 += Math.Pow(x, 4);
+                                sx3 += Math.Pow(x, 3);
+                                sx2 += Math.Pow(x, 2);
+                                sx += x;
+                                sx2y += Math.Pow(x, 2) * y;
+                                sxy += x * y;
+                                sy += y;
+                                n += 1;
+                                s1_x2 += Math.Pow((1 / x), 2);
+                                s1_x += 1 / x;
+                                sy_x += y / x;
+
+                                DataRow newRow = newDataTable.NewRow();
+                                newRow[0] = x;
+                                newRow[1] = y;
+                                newDataTable.Rows.Add(newRow);
+                            }
+                        }
+                    }
+
+                    (double qA, double qB, double qC, double hA, double hB) =
+                        CalcFunctions(sx4, sx3, sx2, sx2y, sx, sxy, sy, n, s1_x, sy_x, s1_x2);
+
+                    PlotModel plotModel = new PlotModel();
+
+                    LinearAxis horizontalAxis = new LinearAxis()
+                        { Position = AxisPosition.Bottom, Minimum = minX - 5 ?? -1, Maximum = maxX + 5 ?? 1, IsZoomEnabled = false, Title = "X", MajorGridlineStyle = LineStyle.Solid, MajorStep = 1};
+                    plotModel.Axes.Add(horizontalAxis);
+                    
+                    LinearAxis verticalAxis = new LinearAxis()
+                        { Position = AxisPosition.Left, Minimum = minY - 5 ?? -1, Maximum = maxY + 5 ?? 1 , IsZoomEnabled = false, Title = "Y", MajorGridlineStyle = LineStyle.Solid, MajorStep = 1};
+                    plotModel.Axes.Add(verticalAxis);
+                    
+                    plotModel.Series.Add(new FunctionSeries(x => qA * x * x + qB * x + qC, minX - 5 ?? -1,
+                        maxX + 5 ?? 1, 0.1, "Quadratic"){Color = OxyColors.Green});
+                    
+                    plotModel.Series.Add(new FunctionSeries(x => hA + hB / x, minX - 5 ?? -1,
+                        -0.1, 0.1, "Hyperbolic"){Color = OxyColors.Blue});
+                    
+                    plotModel.Series.Add(new FunctionSeries(x => hA + hB / x, 0.1,
+                        maxX + 5 ?? 1, 0.1, "Hyperbolic"){Color = OxyColors.Blue});
+
+                    LineSeries dots= new LineSeries()
+                    {
+                        Color = OxyColors.Transparent,
+                        MarkerFill = OxyColors.Red,
+                        MarkerType = MarkerType.Circle,
+                        Title = "Dots"
+                    };
+
+                    foreach (Point point in points)
+                    {
+                        dots.Points.Add(new DataPoint(point.X, point.Y));
+                    }
+                    plotModel.Series.Add(dots);
+                    
+                    GraphModel = plotModel;
+                    
+                    Matrix = newDataTable.DefaultView;
+                }
+            });
+        }
+    }
+
+    public PlotModel GraphModel
+    {
+        get => graphModel ?? new PlotModel();
+        set
+        {
+            graphModel = value;
+            OnPropertyChanged("GraphModel");
+        }
+    }
+
+    private (double, double, double, double, double) CalcFunctions(double sx4,
+        double sx3,
+        double sx2,
+        double sx2y,
+        double sx,
+        double sxy,
+        double sy,
+        double n,
+        double s1_x,
+        double sy_x,
+        double s1_x2)
+    {
+        double hA = (sy * s1_x2 - s1_x * sy_x) / (n * s1_x2 - s1_x * s1_x);
+
+        double hB = (n * sy_x - s1_x * sy) / (n * s1_x2 - s1_x * s1_x);
+
+        GaussMethod gaussMethod = new GaussMethod(3, 3)
+        {
+            Matrix = new double[][]
+            {
+                new double[] { sx4, sx3, sx2 },
+                new double[] { sx3, sx2, sx },
+                new double[] { sx2, sx, n }
+            },
+            RightPart = new double[]
+            {
+                sx2y, sxy, sy
+            }
+        };
+
+        gaussMethod.SolveMatrix();
+
+        double qA = gaussMethod.Answer[0];
+        double qB = gaussMethod.Answer[1];
+        double qC = gaussMethod.Answer[2];
+
+
+        return (qA, qB, qC, hA, hB);
+    }
+}
